e857942158818e37f40a5c5e77742323389e4046
1SpookyGamer <1spookygamer@gmail.com>
2024-04-10 11:13:13 -0400

    did some stufffffff

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text("books/frankenstein.txt")
+    count = count_words("books/frankenstein.txt")
+    alphabet=letter_count("books/frankenstein.txt")
     print(text)
+    print(len(count))
+    print(alphabet)
+    for key in alphabet.keys():
+        print(key, ":", alphabet[key])
+
 
 
 def get_book_text(path):
     with open("books/frankenstein.txt") as f:
         return f.read()
+
+def count_words(path):
+    
+    with open("books/frankenstein.txt") as f:
+        book=f.read()
+        mycount=book.split()
+        return mycount
+
+def letter_count(path):
+    with open("books/frankenstein.txt") as f:
+        book=f.read()
+        lowers=book.lower()
+        res={}
+        for keys in book:
+            res[keys]=res.get(keys,0)+1
+    return res
+
+
 main()
